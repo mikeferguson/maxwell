e68c9f0846c5171f76e3624b524389c3ae5ab8c5
Michael Ferguson <mfergs7@gmail.com>
2014-12-31 18:35:03 -0800

    moving towards robot_calibration

--- maxwell_calibration/view_results/view_scatter.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-rosrun calibration_estimation error_visualization.py /tmp/maxwell_calibration/cal_measurements.bag /tmp/maxwell_calibration/ `rospack find maxwell_calibration`/view_results/scatter_config.yaml
